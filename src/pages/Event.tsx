23a5ddbb3e7473746d01af9a34064218bbd3b96f
Alex Paklin <alexander.pak@prpl.global>
2021-09-16 12:51:25 +0500

    store

---
+++ src/pages/Event.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export const Event: React.FC = (props: Props) => {
+  return <div>EVENT</div>;
+};
---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export const Login: React.FC = (props: Props) => {
+  return <div>LOGIN</div>;
+};
